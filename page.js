7c2c5a50715c9649f50a9e528c5bb23361b6ecaf
Andy Bond <20341309+andyb3@users.noreply.github.com>
2021-02-05 20:53:19 +0000

    tweak banner ids and classes

--- page.js
+++ page.js
@@ -4,16 +4,15 @@ function addBanner(data) {
   if (location.host in data.hosts) {
     // create the banner
     var banner = document.createElement("div");
-    banner.className = "banner-alert";
-    banner.id = "banner";
+    banner.id = "host-banner";
     banner.innerText = data.hosts[location.host]["text"];
     banner.style.backgroundColor = data.hosts[location.host]["colour"];
     // insert banner at top of the page
     document.body.insertBefore(banner, document.body.childNodes[0]);
     if (data.hosts[location.host]["display"] === "insert") {
-      insert_banner(document.getElementById("banner"));
+      insert_banner(document.getElementById("host-banner"));
     } else if (data.hosts[location.host]["display"] === "on_top") {
-      overlay_banner(document.getElementById("banner"));
+      overlay_banner(document.getElementById("host-banner"));
     }
   }
 }
@@ -22,19 +21,19 @@ function insert_banner(banner) {
   // Get the offset position of the banner
   var sticky = banner.offsetTop;
   // When the user scrolls the page, execute stickyBanner so banner remains visible (see https://www.w3schools.com/howto/howto_js_sticky_header.asp)
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky-banner" when you leave the scroll position
   window.onscroll = function () {
     if (window.pageYOffset > sticky) {
-      banner.classList.add("sticky");
+      banner.classList.add("sticky-banner");
     } else {
-      banner.classList.remove("sticky");
+      banner.classList.remove("sticky-banner");
     }
   }
 }
 
 function overlay_banner(banner) {
   // add sticky class to fix at top
-  banner.classList.add("sticky");
+  banner.classList.add("sticky-banner");
   // get the banner height from top of page
   var height = banner.offsetHeight;
   // When mouse over the banner, move the banner to the bottom
